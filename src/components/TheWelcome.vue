ac150982aee3c1776d38fa3f9e1fd9fc0c29980c
ronyman.com <ronyman.com@outlook.com>
2023-07-19 10:58:55 +1000

    initial commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Regular</template>
+
+    $20
+    <a href="#" target="_blank" rel="noopener">Book Now</a>.
+  </WelcomeItem>
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Regular Fade</template>
+
+    $25
+    <a href="#" target="_blank" rel="noopener">Book Now</a>.
+  </WelcomeItem><WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Fade(Taper-Zero-Skin</template>
+
+    $27-$30
+    <a href="#" target="_blank" rel="noopener">Book Now</a>.
+  </WelcomeItem>
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Beard (Line-Trim-Fade</template>
+
+    $10-$20
+    <a href="#" target="_blank" rel="noopener">Book Now</a>.
+  </WelcomeItem>
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Hair Wash</template>
+
+    $10
+    <a href="#" target="_blank" rel="noopener">Book Now</a>.
+  </WelcomeItem>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+  <h1 class="tag is-danger is-large">0412795479</h1>
+    <TheWelcome />
+  </main>
+</template>
+
+
